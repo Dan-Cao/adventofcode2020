73f23fffeb7f97ece809b7c82697813cf9ac6462
Dan Cao <hello@danielcao.com>
2020-12-01 22:57:13 +0000

    tidy up code some more

--- src/advent_of_code_2020/day_01.clj
+++ src/advent_of_code_2020/day_01.clj
@@ -2,50 +2,27 @@
   (:use [clojure.math.combinatorics])
   )
 
-(defn read_file [resource-name] (clojure.string/split-lines (slurp (clojure.java.io/resource resource-name))))
-(defn totals_2020 [x] (= (apply + x) 2020))
+(defn read-file [resource-name] (clojure.string/split-lines (slurp (clojure.java.io/resource resource-name))))
+(defn read-ints-from-file [resource-name] (map #(Integer/parseInt %) (read-file resource-name)))
+(defn get-combinations [items num] (map (fn [x] (into [], x)) (combinations items num)))
+(defn totals-2020 [x] (= (apply + (into [] x)) 2020))
 
 ; Part 1
 (map (fn [x] (apply * x))
-     (filter totals_2020
-             (into [] (
-                        map (fn [x] (into [], x))
-                            (combinations
-                              (map #(Integer/parseInt %)
-                                   (read_file "day_01_input_short.txt"))
-                              2)
-                            )
-                   )
-             )
-     )
+     (filter totals-2020
+             (get-combinations
+               (read-ints-from-file "day_01_input_short.txt")
+               2)))
 
 (map (fn [x] (apply * x))
-     (filter totals_2020
-             (into [] (
-                        map (fn [x] (into [], x))
-                            (combinations
-                              (map #(Integer/parseInt %)
-                                   (read_file "day_01_input.txt"))
-                              2
-                              )
-                            )
-                   )
-             )
-     )
+     (filter totals-2020
+             (get-combinations
+               (read-ints-from-file "day_01_input.txt")
+               2)))
 
 ; Part 2
 (map (fn [x] (apply * x))
-     (filter totals_2020
-             (into [] (
-                        map (fn [x] (into [], x))
-                            (combinations
-                              (map #(Integer/parseInt %)
-                                   (read_file "day_01_input.txt"))
-                              3
-                              )
-                            )
-                   )
-             )
-     )
-
-
+     (filter totals-2020
+             (get-combinations
+               (read-ints-from-file "day_01_input.txt")
+               3)))
